697ec3aaf2e763322593bad7890c4dcf121e203a
KacperLudwiczak <kacperludwiczak@o2.pl>
2023-05-11 00:19:59 +0200

    Lost Focus

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -10,6 +10,14 @@ const SimpleInput = (props) => {
     setEnteredName(event.target.value);
   };
 
+  const nameInputBlurHandler = (event) => {
+    setEnteredNameTouched(true);
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
+      return;
+    }
+  };
+
   const formSubmissionHandler = (event) => {
     event.preventDefault();
 
@@ -44,6 +52,7 @@ const SimpleInput = (props) => {
           type="text"
           id="name"
           onChange={nameInputChangeHandler}
+          onBlur={nameInputBlurHandler}
           value={enteredName}
         />
         {nameInputIsInvalid && (
